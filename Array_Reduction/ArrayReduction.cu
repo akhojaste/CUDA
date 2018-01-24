ce2c0eeffdb8703bc0f8bd836059bd1ad209baa2
Amir Khojaste <a.khojaste@queensu.ca>
2018-01-24 13:09:30 -0500

    Added the case of more than one block of thread

--- Array_Reduction/ArrayReduction.cu
+++ Array_Reduction/ArrayReduction.cu
@@ -12,7 +12,7 @@
 #include <helper_functions.h> // helper functions for SDK examples
 
 
-__global__ void Array_Reduction_Kernel(float * g_iData)
+__global__ void Array_Reduction_Kernel(float * g_iData, float *g_Intermediat)
 {
 	extern __shared__ float sData[]; //Size is determined by the host
 
@@ -20,14 +20,14 @@ __global__ void Array_Reduction_Kernel(float * g_iData)
 	int tId = threadIdx.x;
 
 	//First every thread in the block puts its value intor the shared memory
-	sData[index] = g_iData[index];
+	sData[tId] = g_iData[index];
 	__syncthreads();
 
 	for (int t = 1; t < blockDim.x; t *= 2)
 	{
-		if (index % (2 * t) == 0) //Even threads or even elements of the array. i.e. thread0, thread2, ...
+		if (tId % (2 * t) == 0) //Even threads or even elements of the array. i.e. thread0, thread2, ...
 		{
-			sData[index] += sData[index + t];
+			sData[tId] += sData[tId + t];
 		}
 
 		//At each iteration of the for loop. Thread wait at this barrier so other threads will read here.
@@ -35,29 +35,46 @@ __global__ void Array_Reduction_Kernel(float * g_iData)
 	}
 
 	//Move the summation to the global memory now
-	g_iData[0] = sData[0];
-
+	if (tId == 0)
+	{
+		g_Intermediat[blockIdx.x] = sData[0];
+	}
+	
+	__syncthreads();
 }
 
 extern "C"
 void Array_Reduction(float *h_ArrayReduction, unsigned int ArraySize, long &lSum)
 {
+	int blocks = 1024;
+	int threadPerBlock = ArraySize / 1024; // 1024 blocks of 1024 threads per each block
+	int iMemSize = threadPerBlock * sizeof(float); // to pass to the cuda kernel so it allocates shared memory there
+
 	//Device array
 	float *d_ArrayReduction = nullptr;
 	checkCudaErrors(cudaMalloc((void **)&d_ArrayReduction, ArraySize * sizeof(float)));
 
+	float *d_ArrayReductionIntermediate = nullptr;
+	checkCudaErrors(cudaMalloc((void **)&d_ArrayReductionIntermediate, blocks * sizeof(float)));
+
+	float *d_ArrayReductionOut = nullptr;
+	checkCudaErrors(cudaMalloc((void **)&d_ArrayReductionOut, 1 * sizeof(float)));
+
 	//Move the data from host to device
 	checkCudaErrors(cudaMemcpy(d_ArrayReduction, h_ArrayReduction, ArraySize * sizeof(float), cudaMemcpyHostToDevice));
 
-	dim3 gridArrayReduction(1, 1, 1);
-	dim3 blockArrayReduction(ArraySize, 1, 1);
-	int iMemSize = ArraySize * sizeof(float); // to pass to the cuda kernel so it allocates shared memory there
+	//First call, each block will compute its summation and will put it in one element of output array
+	//So we get one array of 1024 elements.
+	Array_Reduction_Kernel << <blocks, threadPerBlock, iMemSize >> >(d_ArrayReduction, d_ArrayReductionIntermediate);
 
-	Array_Reduction_Kernel << <gridArrayReduction, blockArrayReduction, iMemSize >> >(d_ArrayReduction);
+	//Second call, we have 1024 elemets left, with 1 block of 1024 elemnts each, we compute the summation.
+	blocks = 1;
+	threadPerBlock = 1024;
+	Array_Reduction_Kernel << <blocks, threadPerBlock, iMemSize >> >(d_ArrayReductionIntermediate, d_ArrayReductionOut);
 
-	checkCudaErrors(cudaMemcpy(h_ArrayReduction, d_ArrayReduction, ArraySize * sizeof(float), cudaMemcpyDeviceToHost));
+	checkCudaErrors(cudaMemcpy(h_ArrayReduction, d_ArrayReductionOut, 1 * sizeof(float), cudaMemcpyDeviceToHost));
 
 	lSum = h_ArrayReduction[0];
 
-	cudaFree(d_ArrayReduction);
+	checkCudaErrors(cudaFree(d_ArrayReduction));
 }
